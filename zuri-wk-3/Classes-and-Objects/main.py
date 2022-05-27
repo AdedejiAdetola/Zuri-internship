5167d2058be0dd643eb2b2cb4f658e2bab27b229
unknown <adetolaadedeji0@gmail.com>
2022-05-27 17:24:16 +0100

    classes -objects"

--- zuri-wk-3/Classes-and-Objects/main.py
+++ zuri-wk-3/Classes-and-Objects/main.py
@@ -1,9 +1,32 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    name = str()
+    age = int()
+    tracks = []
+    score = float()
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
 
+    def change_name(self, name):
+        self.name = str(name)
+        return self.name
 
+    def change_age(self, age):
+        self.age = int(age)
+        return self.age
+
+    def add_track(self, track):
+        li = self.tracks
+        li.append(track)
+        self.tracks = li
+        return li
+
+    def get_score(self):
+        self.score = float()
+        return self.score
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
@@ -12,3 +35,5 @@ Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+
+
